550d596a2580213a0aae52de8a2e9a2207982fe8
tkanta <tarininegi@rediffmail.com>
2013-07-12 15:55:51 -0700

    appId removed from config

--- buttonmanagersample/src/main/java/com/sample/buttonmanager/Configuration.java
+++ buttonmanagersample/src/main/java/com/sample/buttonmanager/Configuration.java
@@ -19,7 +19,6 @@ public class Configuration {
 		configMap.put("acct1.UserName", "jb-us-seller_api1.paypal.com");
 		configMap.put("acct1.Password", "WX4WTU3S8MY44S7F");
 		configMap.put("acct1.Signature", "AFcWxV21C7fd0v3bYYYRCpSSRl31A7yDhhsPUU2XhtMoZXsWHFxu-RWy");
-		configMap.put("acct1.AppId", "APP-80W284485P519543T");
 		
 		return configMap;
 	}
@@ -36,7 +35,6 @@ public class Configuration {
 		configMap.put("acct2.Password", "D6JNKKULHN3G5B8A");
 		configMap.put("acct2.CertKey", "password");
 		configMap.put("acct2.CertPath", "resource/sdk-cert.p12");
-		configMap.put("acct2.AppId", "APP-80W284485P519543T");
 		
 		return configMap;
 	}

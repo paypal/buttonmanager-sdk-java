19adec71ba09eea948a3f9442bc6e89a068fa46e
tkanta <tarininegi@rediffmail.com>
2013-07-15 11:56:56 -0700

    dynamic config update

--- buttonmanagersample/src/main/java/com/sample/ipn/IPNListenerServlet.java
+++ buttonmanagersample/src/main/java/com/sample/ipn/IPNListenerServlet.java
@@ -18,7 +18,6 @@ public class IPNListenerServlet extends HttpServlet{
 	 */
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		// Configuration map containing signature credentials and other required configuration.
 		// For a full list of configuration parameters refer at 
 		// (https://github.com/paypal/adaptivepayments-sdk-java/wiki/SDK-Configuration-Parameters)
 		Map<String,String> mode =  Configuration.getMode();
